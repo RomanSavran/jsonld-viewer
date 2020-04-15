fbf4c568ecda67ffe33efd72af2077351837a354
wvfflife <botmailpas@yahoo.com>
2020-04-15 19:56:17 +0300

    add fetch headers

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,13 @@ class GithubAPI {
 
     getData = async (path: string) => {
         const url = `${this._baseLink}${path}`;
-        const res = await fetch(url);
+        const res = await fetch(url, {
+            method: 'GET',
+            cache: 'no-cache',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
 
         if (!res.ok) {
             return res.status
@@ -15,7 +21,13 @@ class GithubAPI {
     getVocabulary = async (path: string) => {
         const url = `${this._baseLink}${path}`;
 
-        const res = await fetch(url);
+        const res = await fetch(url, {
+            method: 'GET',
+            cache: 'no-cache',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
 
         if (!res.ok) {
             return res.status
